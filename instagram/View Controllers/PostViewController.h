6711e0bfc763e90b18f8a34095b80d1d49dabbcc
Lucy Jiang <lucjia@fb.com>
2019-07-09 13:05:01 -0700

    Created PostViewController files

---
+++ instagram/View Controllers/PostViewController.h
@@ -0,0 +1,17 @@
+//
+//  PostViewController.h
+//  instagram
+//
+//  Created by lucjia on 7/9/19.
+//  Copyright © 2019 lucjia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PostViewController : UIViewController <UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
